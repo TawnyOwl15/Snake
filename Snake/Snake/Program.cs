b29fde096cfb917eaa4d484028bc8dbdee870c2c
TawnyOwl15 <ypugach77@mail.ru>
2016-07-07 16:04:38 +0300

    5 занятие, пробы

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,10 +13,39 @@ namespace Snake
             Point p1 = new Point(1, 2, '*');
             p1.Draw();
 
-            Point p2 = new Point(2, 8, '-');            
+            Point p2 = new Point(2, 8, '-');
+            p2 = p1;
+            p1.x = 15;
+            p1.y = 16;       
             p2.Draw();
+            Console.WriteLine("p1.x = " + p1.x + " p1.y = " + p1.y + " p2.x = " + p2.x + " p2.y = " + p2.y);
+          
+            Console.SetCursorPosition(2, 10);
+
+            int x = 1;
+            Func1(x);
+            Console.WriteLine("Func1 называет x = " + x);
+
+            Move(p1, 5, 5);
+           
 
             Console.ReadLine();
+
+        }
+
+        static void Func1(int X)
+        {
+            X = X + 1;
+        }
+        public static void Move(Point p, int dx, int dy)
+        {
+            p.x = p.x + dx;
+            p.y = p.y + dy;
+            Console.WriteLine("p.x = " + p.x + "; p.y = " + p.y);
+        }
+        public static void Reset(Point p)
+        {
+            p = new Point(10, 10, '+');
         }
     }
 }
